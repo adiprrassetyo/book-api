935b22a8923c9b87f0a9d44b24da8557f966733f
adeeep14 <disconetct@gmail.com>
2023-06-10 14:56:22 +0700

    2

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { BooksModule } from './books/books.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), BooksModule, UsersModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), BooksModule, UsersModule, AuthModule],
 })
 export class AppModule {}
